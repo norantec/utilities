a95b52a35d22fdcd755d281d73aee146bbb59b03
lenconda <i@lenconda.top>
2025-03-13 20:57:02 +0800

    fix: 🐛 use cjs

--- src/string-util.class.ts
+++ src/string-util.class.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid/dist/cjs-browser';
 
 export class StringUtil {
     public static isFalsyString(value: any, connector: 'AND' | 'OR' = 'AND') {
