eb443edae1d2fe38b9532f11e70b27f926cfd780
lenconda <i@lenconda.top>
2025-03-14 10:35:43 +0800

    fix: 🐛 browserify

--- src/uuid-util.class.ts
+++ src/uuid-util.class.ts
@@ -1,28 +1,24 @@
 export class UUIDUtil {
     public static generateV4() {
-        if (typeof window !== 'undefined' && typeof window?.crypto?.randomUUID === 'function') {
-            return window.crypto.randomUUID();
+        let getRandomValues: (buffer: Uint8Array) => Uint8Array;
+
+        if (typeof window !== 'undefined' && typeof window.crypto?.getRandomValues === 'function') {
+            getRandomValues = window.crypto.getRandomValues.bind(window.crypto);
         } else {
-            try {
-                return require('crypto').randomUUID();
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            } catch (e) {}
+            const crypto = require('crypto-browserify');
+            getRandomValues = (buffer: Uint8Array) => {
+                return new Uint8Array(crypto.randomFillSync(buffer));
+            };
         }
 
-        const hex: string[] = [];
         const bytes = new Uint8Array(16);
+        getRandomValues(bytes);
 
-        for (let i = 0; i < 16; i++) {
-            bytes[i] = Math.floor(Math.random() * 256);
-        }
+        bytes[6] = (bytes[6] & 0x0f) | 0x40; // 版本号4
+        bytes[8] = (bytes[8] & 0x3f) | 0x80; // 变体
 
-        bytes[6] = (bytes[6] & 0x0f) | 0x40;
-        bytes[8] = (bytes[8] & 0x3f) | 0x80;
-
-        for (let i = 0; i < 16; i++) {
-            hex.push(bytes[i].toString(16).padStart(2, '0'));
-        }
-
-        return `${hex[0]}${hex[1]}${hex[2]}${hex[3]}-${hex[4]}${hex[5]}-${hex[6]}${hex[7]}-${hex[8]}${hex[9]}-${hex[10]}${hex[11]}${hex[12]}${hex[13]}${hex[14]}${hex[15]}`;
+        return [...bytes]
+            .map((b, i) => ([4, 6, 8, 10].includes(i) ? '-' : '') + b.toString(16).padStart(2, '0'))
+            .join('');
     }
 }
