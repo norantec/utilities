ade662bc573ba707fcf962a789c52e747720e8c0
lenconda <i@lenconda.top>
2025-06-11 12:28:13 +0800

    feat: 🎸 add ValidateUtil.cast method

--- src/validate-util.class.ts
+++ src/validate-util.class.ts
@@ -1,4 +1,5 @@
 import { StringUtil } from './string-util.class';
+import { InferType, Schema } from 'yup';
 
 export class ValidateUtil {
     public static isValidIPv4Address(address: string) {
@@ -7,5 +8,16 @@ export class ValidateUtil {
             /^(\d{1,3}\.){3}\d{1,3}$/.test(address || '') &&
             address!.split('.').every((segment) => Number(segment) >= 0 && Number(segment) <= 255)
         );
+    }
+
+    /**
+     * Casts and validates an input based on the provided Yup schema.
+     * Returns the fully casted and validated result.
+     * Throws if validation fails.
+     */
+    public static cast<T extends Schema<any>>(schema: T, input: unknown): InferType<T> {
+        const casted = schema.cast(input);
+        schema.validateSync(casted); // throws if invalid
+        return casted as InferType<T>;
     }
 }
