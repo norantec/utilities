01d0fc2dd8998561f723a5032d51f73e70576b52
lenconda <i@lenconda.top>
2025-08-22 14:55:43 +0800

    fix: 🐛 issues

--- src/log-util.class.ts
+++ src/log-util.class.ts
@@ -3,8 +3,9 @@ import { Schema } from './schema-util.class';
 const LOG_LEVELS: Schema.LogLevel[] = ['debug', 'verbose', 'info', 'warn', 'error'];
 
 export class LogUtil {
-    public match(bottomLogLevel: Schema.LogLevel | false, logLevel: Schema.LogLevel) {
+    public match(bottomLogLevel: Schema.LogLevel | boolean, logLevel: Schema.LogLevel) {
         if (bottomLogLevel === false) return false;
-        return LOG_LEVELS.slice(0, LOG_LEVELS.findIndex((level) => level === bottomLogLevel) + 1).includes(logLevel);
+        if (bottomLogLevel === true) return true;
+        return LOG_LEVELS.slice(LOG_LEVELS.findIndex((level) => level === bottomLogLevel)).includes(logLevel);
     }
 }
