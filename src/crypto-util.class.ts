efa602f863c7e509345746bfb11364ac602572ac
lenconda <i@lenconda.top>
2025-09-28 11:55:57 +0800

    feat: 🎸 add new methods

--- src/crypto-util.class.ts
+++ src/crypto-util.class.ts
@@ -1,11 +1,11 @@
 export class CryptoUtil {
-    public static async importRsaPkcs8Pem(
+    public static async importRsaPrivatePemKey(
         pemString: string,
         algorithm: AlgorithmIdentifier,
         keyUsages: Iterable<KeyUsage>,
     ) {
         // Remove PEM headers, footers, and newlines
-        const base64 = pemString.replace(/(-----(BEGIN|END) (PUBLIC|PRIVATE) KEY-----|\n|\r)/g, '').trim();
+        const base64 = pemString.replace(/(-----(BEGIN|END) PRIVATE KEY-----|\n|\r)/g, '').trim();
 
         // Base64 decode to binary string
         const binaryString = atob(base64);
@@ -21,6 +21,37 @@ export class CryptoUtil {
         // Import the key
         const importedKey = await crypto.subtle.importKey(
             'pkcs8',
+            keyArrayBuffer,
+            algorithm,
+            true, // extractable
+            keyUsages,
+        );
+
+        return importedKey;
+    }
+
+    public static async importRsaPublicPemKey(
+        pemString: string,
+        algorithm: AlgorithmIdentifier,
+        keyUsages: Iterable<KeyUsage>,
+    ) {
+        // Remove PEM headers, footers, and newlines
+        const base64 = pemString.replace(/(-----(BEGIN|END) PUBLIC KEY-----|\n|\r)/g, '').trim();
+
+        // Base64 decode to binary string
+        const binaryString = atob(base64);
+
+        // Convert binary string to ArrayBuffer
+        const keyArrayBuffer = new ArrayBuffer(binaryString.length);
+        const keyArrayBufferView = new Uint8Array(keyArrayBuffer);
+
+        for (let i = 0; i < binaryString.length; i += 1) {
+            keyArrayBufferView[i] = binaryString.charCodeAt(i);
+        }
+
+        // Import the key
+        const importedKey = await crypto.subtle.importKey(
+            'spki',
             keyArrayBuffer,
             algorithm,
             true, // extractable
